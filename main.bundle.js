77462f545cc8a7dc5bcd5c4ac3f4ef41dbf9702f
Bodor Gergely <bodorgergely91@gmail.com>
2017-03-19 23:34:06 +0100

    dist subtree commit

--- main.bundle.js
+++ main.bundle.js
@@ -8210,12 +8210,9 @@ var AppModule = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
 var environment = {
-    production: false
+    production: true,
+    baseURL: '/time-logger'
 };
 //# sourceMappingURL=/home/bgolyoo/work/hub-repo/time-logger/src/environment.js.map
 
